5cb19db34755a21fbd5d60b099c0f78ea746303d
mehmetAltinpinar <mehmetu7@gmail.com>
2023-03-24 20:07:17 -0400

    TCs completed

--- src/test/java/Runners/RunnerForTC_009_010_011_012.java
+++ src/test/java/Runners/RunnerForTC_009_010_011_012.java
@@ -4,6 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
+//        tags = "";
         features = {"src/test/java/featureFiles/TC_RF_009_010_011_012.feature"},
         glue = {"stepDefinitions"}
 )
