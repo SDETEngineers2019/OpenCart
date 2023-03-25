e930e9e59c340293c13967bea9aa733457e2e107
user <sevgi012127@gmail>
2023-03-25 10:03:41 -0600

    I created new

---
+++ src/test/java/pages/RegisterPageElement_Sevgi.java
@@ -0,0 +1,139 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.BaseDriver;
+import utilities.MyMethods;
+
+import java.util.List;
+
+public class RegisterPageElement_Sevgi extends MyMethods {
+    public RegisterPageElement_Sevgi() {
+        PageFactory.initElements(BaseDriver.getDriver(), this);
+
+    }
+    @FindBy(xpath = "//input[@id='input-firstname']")
+    private WebElement registerFirstNameInput;
+
+    @FindBy(xpath = "//input[@id='input-lastname']")
+    private WebElement registerLastNameInput;
+
+    @FindBy(xpath = "//input[@id='input-email']")
+    private WebElement registerEmailInput;
+
+    @FindBy(xpath = "//input[@id='input-telephone']")
+    private WebElement telephoneBtn;
+    @FindBy(xpath = "//input[@id='input-password']")
+    private WebElement registerPasswordInput;
+    @FindBy(xpath = "//input[@id='input-confirm']")
+    private WebElement registerPasswordConfirm;
+
+    @FindBy(xpath = "(//label[@class='radio-inline'])[2]")
+    private WebElement newLetterBtn;
+
+    @FindBy(xpath="//input[@type='checkbox']")
+    private WebElement registerCheckBox;
+
+    @FindBy(xpath = "//input[@type='submit']")
+    private WebElement registerContinueButton;
+
+    @FindBy(xpath = "//div[text()=' Warning: You must agree to the Privacy Policy!']")
+    private WebElement warningText;
+
+    @FindBy(xpath = "//div[text()=' Warning: You must agree to the Privacy Policy!']")
+    private WebElement warningText2;
+
+    @FindBy(linkText = "login page")
+    private WebElement linkTextBtn;
+
+    @FindBy(xpath ="//div[@class='list-group']/a")
+    private List<WebElement> rightColumnBtn;
+
+    @FindBy(xpath = "//ul[@class='dropdown-menu']/li")
+    private List<WebElement> menuOption1;
+
+    @FindBy(xpath = "//div[@class='row']")
+    private List<WebElement> headerBtn;
+
+    @FindBy(xpath = "//div[@class='container']")
+    private List<WebElement> footerBtn;
+
+    @FindBy(xpath = "//ul[@class='list-inline']/li")
+    private List<WebElement> topListBtn;
+
+
+
+
+
+
+    public WebElement getRegisterFirstNameInput() {
+        return registerFirstNameInput;
+    }
+
+    public WebElement getRegisterLastNameInput() {
+        return registerLastNameInput;
+    }
+
+    public WebElement getRegisterEmailInput() {
+        return registerEmailInput;
+    }
+
+    public WebElement getTelephoneBtn() {
+        return telephoneBtn;
+    }
+
+    public WebElement getRegisterPasswordInput() {
+        return registerPasswordInput;
+    }
+
+    public WebElement getRegisterPasswordConfirm() {
+        return registerPasswordConfirm;
+    }
+
+    public WebElement getNewLetterBtn() {
+        return newLetterBtn;
+    }
+
+    public WebElement getRegisterCheckBox() {
+        return registerCheckBox;
+    }
+
+    public WebElement getRegisterContinueButton() {
+        return registerContinueButton;
+    }
+
+    public WebElement getWarningText() {
+        return warningText;
+    }
+
+    public WebElement getWarningText2() {
+        return warningText2;
+    }
+
+
+
+    public List<WebElement> getHeaderBtn() {
+        return headerBtn;
+    }
+
+    public List<WebElement> getFooterBtn() {
+        return footerBtn;
+    }
+
+    public List<WebElement> getTopListBtn() {
+        return topListBtn;
+    }
+
+    public WebElement getLinkTextBtn() {
+        return linkTextBtn;
+    }
+
+    public List<WebElement> getRightColumnBtn() {
+        return rightColumnBtn;
+    }
+
+    public List<WebElement> getMenuOption1() {
+        return menuOption1;
+    }
+}
---
+++ src/test/java/stepDefinitions/Sevgi/TC_RF_021Steps.java
@@ -0,0 +1,71 @@
+package stepDefinitions.Sevgi;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import pages.RegisterPageElement_Sevgi;
+import pages.RegisterPageElements;
+
+public class TC_RF_021Steps {
+
+    RegisterPageElements rp = new RegisterPageElements();
+    RegisterPageElement_Sevgi rps= new RegisterPageElement_Sevgi();
+
+    @Given("Fill out all the fields")
+    public void fill_out_all_the_fields() {
+//
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        rps.sendKeysMethod(rps.getRegisterFirstNameInput(),"Sam");
+//   rps.sendKeysMethod(rps.getRegisterLastNameInput(),"Jhonny");
+//   rps.sendKeysMethod(rps.getRegisterEmailInput(),"Sam123@com");
+//   rps.sendKeysMethod(rps.getTelephoneBtn(),"1234566");
+//   rps.sendKeysMethod(rps.getRegisterPasswordInput(),"123qwe");
+//   rps.sendKeysMethod(rps.getRegisterPasswordConfirm(),"123qwe");
+//
+
+    }
+    @When("Click on the Continue Button")
+    public void click_on_the_continue_button() {
+
+        rps.clickMethod(rps.getNewLetterBtn());
+        //rp.clickMethod(rp.getRegisterCheckBox());
+
+        rps.clickMethod(rps.getRegisterContinueButton());
+    }
+    @Then("user should be Warning Text")
+    public void user_should_be_warning_text() {
+        waitUntilVisible(rps.getWarningText());
+    Assert.assertTrue(rps.getWarningText().isDisplayed());
+    }
+
+    private void waitUntilVisible(WebElement warningText) {
+    }
+
+
+    @Given("Fill out all the fields without click checkBox")
+    public void fillOutAllTheFieldsWithoutClickCheckBox() {
+
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        rps.sendKeysMethod(rps.getRegisterFirstNameInput(),"Sam");
+        rps.sendKeysMethod(rps.getRegisterLastNameInput(),"Jhonny");
+        rps.sendKeysMethod(rps.getRegisterEmailInput(),"Sam123@gmail.com");
+        rps.sendKeysMethod(rps.getTelephoneBtn(),"1234566");
+        rps.sendKeysMethod(rps.getRegisterPasswordInput(),"123qwe");
+        rps.sendKeysMethod(rps.getRegisterPasswordConfirm(),"123qwe");
+
+    }
+}
