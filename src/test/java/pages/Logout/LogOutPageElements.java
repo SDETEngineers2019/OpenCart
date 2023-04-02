64f590604c105e9d9c6e494061ff116c2e42a75c
ayse <sabriyebeskardes@gmail.com>
2023-04-02 01:23:20 -0400

    log out page webelements some change made.

--- src/test/java/pages/Logout/LogOutPageElements.java
+++ src/test/java/pages/Logout/LogOutPageElements.java
@@ -13,38 +13,38 @@ public class LogOutPageElements extends MyMethods {
         PageFactory.initElements(BaseDriver.getDriver(), this);
     }
 
-    @FindBy(css = "#input-email")
+    @FindBy(css = "#input-email")  // login page email input box
     private WebElement loginEmailInput;
 
-    @FindBy (css = "#input-password")
+    @FindBy (css = "#input-password")    //login page password input box
     private WebElement loginPasswordInput;
-    @FindBy(xpath = "//input[@type='submit']")
+    @FindBy(xpath = "//input[@type='submit']")  // login page login button
     private WebElement loginButton;
 
-    @FindBy(xpath = "(//div[@class='list-group']/a)[13]")
+    @FindBy(xpath = "(//div[@class='list-group']/a)[13]")  //Log out from right column
     private WebElement logOutButton;
 
     @FindBy(xpath = "//div[@id='content']/p[1]")
-    private WebElement successfullyLoggedOutMessage;
+    private WebElement successfullyLoggedOutMessage; //When you successfully log out from right column, this message shows up.
 
-    @FindBy(xpath = "//div[@class='pull-right']")
+    @FindBy(xpath = "//div[@class='pull-right']")   //When you log out from right column, this is that continue button that takes you to home page
     private WebElement logOutContinueButton;
 
     @FindBy(id = "logo")
-    private WebElement homePage;
+    private WebElement homePage; // When you click continue logout,this home page will be shown.
 
-    @FindBy(xpath = "(//span[@class='hidden-xs hidden-sm hidden-md'])[3]")
+    @FindBy(xpath = "(//span[@class='hidden-xs hidden-sm hidden-md'])[3]")  // My Account dropdown button on top of the page right side.
     private WebElement myAccountDropDownButton;
 
-    @FindBy(xpath = "//*[text()='Logout']")
+    @FindBy(xpath = "//*[text()='Logout']")  //Log out button from My Account dropdown on the top of the page.
     private WebElement logOutFromDropDown;
 
-    @FindBy(xpath = "(//*[@class='breadcrumb']//li/a)[3]")
+    @FindBy(xpath = "(//*[@class='breadcrumb']//li/a)[3]")  //When you log out from My Account dropdown, this Logout breadcrumb will appear.
     private WebElement logOutBreadCrumb;
-    @FindBy(xpath = "//h1[text()='Account']")
+    @FindBy(xpath = "//h1[text()='Account']") //When you logout from My Account dropdown ,this "Account" header will appear.
     private WebElement logOutPageHeader;
 
-    @FindBy(xpath = "(//*[text()='Login'])[1]")
+    @FindBy(xpath = "(//*[text()='Login'])[1]") // Login button from My Account dropdown from the top of the page.
     private WebElement loginFromDropDown;
 
     public WebElement getLogOutButton() {
