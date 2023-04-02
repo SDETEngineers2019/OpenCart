e83e0ac3bba596d16d93af3ca249a644aa70d5bf
mehmetAltinpinar <mehmetu7@gmail.com>
2023-04-02 02:00:33 -0400

    1. user story regarding the Logout functionality were finished

--- src/test/java/stepDefinitions/Logout/TC_LG_001_008.java
+++ src/test/java/stepDefinitions/Logout/TC_LG_001_008.java
@@ -3,13 +3,49 @@ package stepDefinitions.Logout;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.Keys;
-import org.testng.Assert;
-import pages.Register.ElementsForTC_009_010_011_012;
-import pages.Register.RegisterPageElements;
-
-import java.awt.*;
+import pages.Logout.LogOutPageElements;
 
 public class TC_LG_001_008 {
+    LogOutPageElements lp = new LogOutPageElements();
+    @When("Click on My Account Dropdown Menu")
+    public void clickOnMyAccountDropdownMenu() {
+        lp.clickMethod(lp.getMyAccountDropDownButton());
+    }
+
+    @And("Click on Logout Button")
+    public void clickOnLogoutButton() {
+        lp.clickMethod(lp.getLogOutFromDropDown());
+    }
+
+    @Then("User should be taken to Account Logout page")
+    public void userShouldBeTakenToAccountLogoutPage() {
+        lp.verifyContainsText(lp.getSuccessfullyLoggedOutMessage(), "logged off");
+    }
+
+    @And("User should see Login Option under My Account Dropdown Menu")
+    public void userShouldSeeLoginOptionUnderMyAccountDropdownMenu() {
+        lp.clickMethod(lp.getMyAccountDropDownButton());
+        lp.isDisplayed(lp.getLoginFromDropDown());
+    }
+
+    @And("Click on Logout Continue Button")
+    public void clickOnLogoutContinueButton() {
+        lp.clickMethod(lp.getLogOutContinueButton());
+    }
+
+    @Then("User should be taken to Home page")
+    public void userShouldBeTakenToHomePage() {
+        lp.verifyURL("http://opencart.abstracta.us/index.php?route=common/home");
+    }
+
+    @And("Click on Login button under My Account Dropdown Menu")
+    public void clickOnLoginButtonUnderMyAccountDropdownMenu() {
+        lp.clickMethod(lp.getLoginFromDropDown());
+    }
+
+    @Then("User should be taken to Login Page")
+    public void userShouldBeTakenToLoginPage() {
+        lp.verifyURL("https://opencart.abstracta.us/index.php?route=account/login");
+    }
 
 }
