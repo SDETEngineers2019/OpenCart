d6a07a8ded56126c1991813b211b71bd0a8b476f
ayse <sabriyebeskardes@gmail.com>
2023-04-03 17:22:34 -0400

    ForgotPasswordWebElements

--- src/test/java/pages/ForgotPassword/ForgotPasswordWebElements.java
+++ src/test/java/pages/ForgotPassword/ForgotPasswordWebElements.java
@@ -1,28 +1,35 @@
 package pages.ForgotPassword;
 
-import org.apache.commons.compress.archivers.zip.X000A_NTFS;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.BaseDriver;
+import utilities.MyMethods;
 
-public class ForgotPasswordWebElements extends BaseDriver {
 
-    public ForgotPasswordWebElements() {
-        PageFactory.initElements(BaseDriver.getDriver(), this);
+
+public class ForgotPasswordWebElements extends MyMethods {
+
+    public ForgotPasswordWebElements (){
+      PageFactory.initElements(BaseDriver.getDriver(),this);
 
     }
 
     @FindBy(css = "#input-email")
-    private WebElement forgotPasswordEmailInput;
-    @FindBy(xpath = "//input[@type='submit']")
+    private WebElement forgotPasswordEmailInput; //When you click on forgotten password button,this e-mail input shows up.
+    @FindBy(xpath = "//input[@type='submit']") //When you enter you e-mail to inbox,then continue button
     private WebElement forgotPasswordContinueButton;
-    @FindBy(className = "alert alert-success alert-dismissible")
+    @FindBy(css= ".alert.alert-success.alert-dismissible") //When you enter valid e-mail address this message will show up.
     private WebElement confirmationLinkHasBeenSentMessage;
-    @FindBy(xpath = "(//a[text()='Forgotten Password'])[1]")
+
+    @FindBy(xpath = "(//a[text()='Forgotten Password'])[1]") //under the password input on login page
     private WebElement forgottenPasswordUnderPasswordInput;
-    @FindBy(xpath = "(//a[text()='Forgotten Password'])[2]")
+
+   @FindBy(xpath = "(//a[text()='Forgotten Password'])[2]") //Right side of the page
     private WebElement forgottenPasswordRightColumn;
+   @FindBy(xpath = "//div[@class='alert alert-danger alert-dismissible']")//When you enter invalid email to forgotten password reset input,this message will show up
+   private WebElement warningMessage;
 
 
     public WebElement getForgotPasswordEmailInput() {
@@ -43,5 +50,10 @@ public class ForgotPasswordWebElements extends BaseDriver {
 
     public WebElement getForgottenPasswordRightColumn() {
         return forgottenPasswordRightColumn;
+    }
+
+    public WebElement getWarningMessage() {
+        return warningMessage;
+
     }
 }
